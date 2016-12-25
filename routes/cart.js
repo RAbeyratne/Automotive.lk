946e105e82d19357a794b7fbd392ab2add66c956
Ramitha Abeyratne <ramithaabeyratne@rocketmail.com>
2016-12-25 19:52:06 +0530

    Fixed a major issue with the cart

--- routes/cart.js
+++ routes/cart.js
@@ -7,9 +7,7 @@ var globals = require('../globals');
 router.use(session({secret:'ydfuisyfviusydgvi1ugnlk', resave:false, saveUninitialized:true}));
 
 // Add item to cart
-router.post('/additem', function (req, res) {    
-    
-//    console.info(req.body.item.qty);
+router.post('/additem', function (req, res) {  
     var dataItem = req.body.item;
     dataItem['qty'] = Number(req.body.qty);
     dataItem['totalAmount'] = req.body.item.price * req.body.qty;
@@ -17,32 +15,32 @@ router.post('/additem', function (req, res) {
 
     var isInsideCart = false;
     
+    // Checking whether the item is inside the array
     for(var itemInCart in globals.shoppingCart){
-        if (itemInCart == req.body.item.pid){
+        if (itemInCart == req.body.item.pid){   
             isInsideCart = true;
-            globals.shoppingCart[itemInCart].qty = globals.shoppingCart[itemInCart].qty + dataItem.qty;
-            console.log('remaining number qty ' + globals.shoppingCart[itemInCart].qty);
-            console.log('new number qty ' + dataItem.qty);
             
-            var total = globals.shoppingCart[itemInCart].qty + dataItem.qty;
-            console.log('total number qty ' + total);
-            
-//            res.status(200).send(globals.shoppingCart[itemInCart].qty);
+            // If so, updating the quantity and total amount of the item
+            var remainingQty = globals.shoppingCart[itemInCart].qty;    
+            var remainingTotal = globals.shoppingCart[itemInCart].totalAmount;   
+            globals.shoppingCart[itemInCart].qty = remainingQty + dataItem.qty;  
+            globals.shoppingCart[itemInCart].totalAmount = remainingTotal + dataItem.totalAmount;
+            res.status(200).send('Updated the content of the cart');            
+            break;
         }      
     }
-    globals.shoppingCart[req.body.item.pid] = req.body.item;
-    
+
     if (!isInsideCart){
-        globals.shoppingCart[ dataItem['pid']] = dataItem;
-        res.status(200).send(globals.shoppingCart);
+        // Making a new item entry, if it's not already in the cart
+        globals.shoppingCart[ dataItem['pid']] = dataItem;        
+        res.status(200).send('Added the new item to the cart');
     }
 });
 
 // Get cart details
 router.get('/status', function (req, res) {
-    res.status(200).send(JSON.stringify(globals.shoppingCart));
+    res.status(200).send(globals.shoppingCart);
 });
-
 
 
 console.log('Cart Router Active');

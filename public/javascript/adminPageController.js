c673573b9759caf690269957367f195ef987cc72
Ramitha Abeyratne <ramithaabeyratne@rocketmail.com>
2017-01-02 05:52:44 +0530

    UI + the controller of the admin page

---
+++ public/javascript/adminPageController.js
@@ -0,0 +1,9 @@
+/**
+ * Automotive.lk
+ */
+var myApp = angular.module('adminPage', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+
+    console.log('Admin page controller active ~~~');
+    
+}]);

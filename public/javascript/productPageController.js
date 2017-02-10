b69f1368a5052526c36d816fc56c74bb7db61d12
Ramitha Abeyratne <ramithaabeyratne@rocketmail.com>
2017-02-10 23:09:26 +0530

    Added the price filtering mechnism for products.

--- public/javascript/productPageController.js
+++ public/javascript/productPageController.js
@@ -2,6 +2,39 @@
  * Automotive.lk
  */
 var myApp = angular.module('productPage', []);
+
+$minPriceAmount = '';
+$maxPriceAmount = '';
+
+myApp.filter('priceFilter',[function (){
+    
+    return function (item) {
+        
+        var newArray = [];
+        
+        if (item != null) {
+            angular.forEach(item, function(value,index){                
+                if (($minPriceAmount == '') || ($maxPriceAmount == '')){ 
+                    if (($minPriceAmount != '') && ($minPriceAmount > value.price)){       
+                           // continue;
+                    } else if (($maxPriceAmount != '') && ($maxPriceAmount < value.price)){     
+                            //continue;
+                    } else {         
+                        newArray.push(value); 
+                    }
+                } else {
+                    if (($minPriceAmount <= value.price) && ($maxPriceAmount >= value.price)){
+                          newArray.push(value);
+                     }
+                }   
+            });
+            return newArray;
+            console.log($minPriceAmount);
+        };
+        
+    };
+}]);
+
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 
     console.log('Product page controller active ~~~');
@@ -20,5 +53,23 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
         window.location = '/item.html?pid=' + productId; 
     };
         
+        
+    $scope.clearSearchFields = function () { 
+        $scope.keyWords = '';   
+    };
+        
+    $scope.updatePriceMinFilter = function () { 
+        //console.log("me");
+        $minPriceAmount = $scope.minFilter; 
+    };
     
+    $scope.updatePriceMinFilter = function () { 
+        $minPriceAmount = $scope.minFilter; 
+    };    
+        
+    $scope.updatePriceMaxFilter = function () { 
+        $maxPriceAmount = $scope.maxFilter; 
+    };
 }]);
+
+
